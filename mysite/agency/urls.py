1bf372fd52189e8b4a64d1f7f67ecab40da2a24b
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2022-06-24 21:43:44 +0200

    Added first HTML files

--- mysite/agency/urls.py
+++ mysite/agency/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-
 from . import views
+
+
+app_name = 'agency'
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('profile/', views.profile, name='profile'),
+    path('festival/', views.festival, name='festival'),
+    path('tickets/', views.tickets, name='tickets'),
+    # path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+] 
--- mysite/agency/views.py
+++ mysite/agency/views.py
@@ -4,3 +4,34 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the agency index.")
+
+
+def profile(request):
+    # return HttpResponse("Your profile is here.")
+    return render(request, 'agency/profile.html')
+
+
+def festival(request):
+    # return HttpResponse("Your festival is here.")
+    return render(request, 'agency/festival.html')
+
+
+def tickets(request):
+    # return HttpResponse("Your tickets are here.")
+    return render(request, 'agency/tickets.html')
+
+
+def genres(request):
+    return HttpResponse("Your genres are here.")
+
+
+def instrumets(request):
+    return HttpResponse("Your instruments are here.")
+
+
+def concerts(request):
+    return HttpResponse("Your concerts are here.")
+
+
+def artists(request):
+    return HttpResponse("Your artists are here.")
