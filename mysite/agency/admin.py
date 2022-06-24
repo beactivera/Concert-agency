aba359475dcff227c27676ca18663fdc2a1d1351
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2022-06-24 21:09:32 +0200

    Added some records into database

--- mysite/agency/admin.py
+++ mysite/agency/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Genre, Festival, Sponsor, Concert, Concert_on_festival, Ticket, Ticket_for_concert, Band, Artist, Instrument, Genre_and_intruments, Client
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Festival)
+admin.site.register(Sponsor)
+admin.site.register(Concert)
+admin.site.register(Concert_on_festival)
+admin.site.register(Ticket)
+admin.site.register(Ticket_for_concert)
+admin.site.register(Band)
+admin.site.register(Artist)
+admin.site.register(Instrument)
+admin.site.register(Genre_and_intruments)
+admin.site.register(Client)
